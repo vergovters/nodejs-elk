ea7a279ffc2e0bb00eefef8443ca025624780689
illia <fialkivskiji@gmail.com>
2024-05-30 23:11:23 +0300

    posts

--- nodejs-app/index.js
+++ nodejs-app/index.js
@@ -1,12 +1,11 @@
 import express from "express";
 import controllers from "./controller.js";
 
+const app = express();
 
-const  app = express()
+app.get("/", controllers.homeController);
+app.get("/posts", controllers.postController);
 
-app.get('/',controllers.homeController)
-app.get('/post',controllers.postController)
-
-app.listen(8080,()=>{
-    console.log("server is running on port 8080")
-})
+app.listen(8080, () => {
+  console.log("server is running on port 8080");
+});
